74f8a4db329e2e36a9a5799f67b0357b225e9ab0
ox0114xo <ox0114xo@gmail.com>
2026-02-25 23:48:05 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,5 @@
-// Service Worker 核心邏輯
-const CACHE_NAME = 'ue-cache-v1';
-
-self.addEventListener('install', (event) => {
-    self.skipWaiting();
-});
-
-self.addEventListener('activate', (event) => {
-    event.waitUntil(clients.claim());
-});
-
-// 必須有 fetch 事件，安裝視窗才會彈出
-self.addEventListener('fetch', (event) => {
-    event.respondWith(fetch(event.request));
+self.addEventListener('install', (e) => self.skipWaiting());
+self.addEventListener('activate', (e) => e.waitUntil(clients.claim()));
+self.addEventListener('fetch', (e) => {
+    e.respondWith(fetch(e.request));
 });
